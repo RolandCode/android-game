e70e055cb05343a501e06d58da06f4daf1bcbe38
Vincenzo Giovanni Comito <clynamen@gmail.com>
2012-11-17 20:57:26 +0100

    added speed, acceleration

--- Game/src/com/example/Vettore.java
+++ Game/src/com/example/Vettore.java
@@ -22,54 +22,66 @@ public final class Vettore {
 		  }
 
 
-		  public Vettore somma(Vettore altro) {
+		  public Vettore add(Vettore altro) {
 		    Vettore ret = new Vettore(); // Creiamo un nuovo vettore. Questo sara` il risultato di questa funzione
 		    ret.x = altro.x + x; // sommiamo le componenti x di questo vettore e dell'altro
 		    ret.y = altro.y + y; // Stessa cosa, con la componente y.
 		    return ret; // diamo il risultato. 
 		  }
 
-		  public Vettore differenza(Vettore altro) {
+		  public Vettore sub(Vettore altro) {
 		    Vettore ret = new Vettore();
-		    ret.x = altro.x - x;
-		    ret.y = altro.y - y;
+		    ret.x = x - altro.x ;
+		    ret.y = y - altro.y;
 		    return ret;
 		  }
 
-		  public Vettore prodotto(Vettore altro) {
+		  public Vettore mul(Vettore altro) {
 		    Vettore ret = new Vettore();
 		    ret.x = altro.x * x;
 		    ret.y = altro.y * y;
 		    return ret;
 		  }
 		  
-		  public Vettore prodotto(float fat) {
-			    Vettore ret = new Vettore();
+		  public Vettore mul(float fat) {
+			    Vettore ret = new Vettore(this);
 			    ret.x *= fat;
 			    ret.y *= fat;
 			    return ret;
 		  }
 		  
-		  public Vettore divisione(Vettore altro) {
+		  public Vettore div(Vettore altro) {
 			Vettore ret = new Vettore();
-			ret.x = altro.x / x;
-		    ret.y = altro.y / y;
+			ret.x = x / altro.x;
+		    ret.y = y / altro.y;
 		    return ret;
 		  }
 		  
-		  public Vettore divisione(float fat) {
-			    Vettore ret = new Vettore();
+		  public Vettore div(float fat) {
+			    Vettore ret = new Vettore(this);
 			    ret.x /= fat;
 			    ret.y /= fat;
 			    return ret;
 		  }
 		  
-		  public Vettore ProdottoPunto(Vettore altro) {
+		  public Vettore dotProduct(Vettore altro) {
 			Vettore ret = new Vettore();
 			// TODO: implementare questo metodo.
 			// usare la funzione Math.cos()
 			//
 			return ret; 
+		  }
+		  
+		  public Vettore normalize() {
+			  int magn = magnitude();
+			  Vettore ret = new Vettore();
+			  ret.x = x / magn;
+			  ret.y = y / magn;
+			  return ret;
+		  }
+		  
+		  public int magnitude() {
+			  return (int) Math.sqrt(x*x + y*y);
 		  }
 		  
 		  public String toString() {
