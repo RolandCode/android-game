d789adfcec4adabed4d6bc3233ce65d5a5736972
Roland Peraj <rolandperaj@gmail.com>
2013-02-03 21:08:16 +0100

    class TexturedCircle

--- Game/src/com/example/TexturedCircle.java
+++ Game/src/com/example/TexturedCircle.java
@@ -9,27 +9,16 @@ import android.graphics.Paint;
 
 public class TexturedCircle extends Circle {
     protected Bitmap texture;
-    protected int imageResource;
-    protected Resources risorse;
-    private boolean flagInit = true;
     private Paint paint = new Paint();
     public int radius;
 
     public TexturedCircle(PhysicsEngine engine, int radius, int imageResource, Resources risorse) {
         super(engine);
-        this.imageResource = imageResource;
-        this.risorse = risorse;
         this.radius = radius;
-        init();
+        this.texture = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(risorse, imageResource), 2*this.radius, 2*this.radius, true);
     }
 
     protected void drawThis(Canvas canvas, Vector position) {
        canvas.drawBitmap(texture, position.x-this.radius, position.y-this.radius, paint);
-    }
-
-    private void init(){
-         if (flagInit){
-             this.texture = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(risorse, this.imageResource), 2*this.radius,2*this.radius, true);
-         } flagInit = false;
     }
 }
